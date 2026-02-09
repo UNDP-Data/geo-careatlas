65c893800ad5b2f36ebb08bded26a33f6ad5d890
Ioan Ferencik <ioan.ferencik@undp.org>
2026-02-09 19:05:21 +0100

    extra headers for ingress

--- src/careatlas/app/server.py
+++ src/careatlas/app/server.py
@@ -5,15 +5,7 @@ import os
 app = FastAPI(title="UNDP CareAtlas API")
 
 @app.get("/")
-@app.get("")
 async def root(
-    # # These headers are injected by the OAuth2 Proxy upon successful GH login
-    # # Try both standard proxy header names
-    # user: Annotated[Optional[str], Header(alias="x-forwarded-user")] = None,
-    # email: Annotated[Optional[str], Header(alias="x-forwarded-email")] = None,
-    # # Backup: The ones you already had
-    # auth_user: Annotated[Optional[str], Header(alias="x-auth-request-user")] = None,
-    # auth_email: Annotated[Optional[str], Header(alias="x-auth-request-email")] = None,
     user: Annotated[Optional[str], Header(alias="x-forwarded-user")] = None,
     email: Annotated[Optional[str], Header(alias="x-forwarded-email")] = None,
     # The groups header name can vary, this is the most common for GH teams
