83f9099f25880782b71e4439c99dcfa50f30f328
Ioan Ferencik <ioan.ferencik@undp.org>
2026-02-09 16:47:09 +0100

    setup oauth2 forwarded headers

--- src/careatlas/app/server.py
+++ src/careatlas/app/server.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from typing import Annotated, Optional
 import os
 
-app = FastAPI(title="Geo-CareAtlas API")
+app = FastAPI(title="UNDP CareAtlas API")
 
 @app.get("/")
-async def root():
+@app.get("")
+async def root(
+    # # These headers are injected by the OAuth2 Proxy upon successful GH login
+    # # Try both standard proxy header names
+    # user: Annotated[Optional[str], Header(alias="x-forwarded-user")] = None,
+    # email: Annotated[Optional[str], Header(alias="x-forwarded-email")] = None,
+    # # Backup: The ones you already had
+    # auth_user: Annotated[Optional[str], Header(alias="x-auth-request-user")] = None,
+    # auth_email: Annotated[Optional[str], Header(alias="x-auth-request-email")] = None,
+    user: Annotated[Optional[str], Header(alias="x-forwarded-user")] = None,
+    email: Annotated[Optional[str], Header(alias="x-forwarded-email")] = None,
+    # The groups header name can vary, this is the most common for GH teams
+    groups: Annotated[Optional[str], Header(alias="x-forwarded-groups")] = None,
+    
+):
     return {
         "app": "UNDP CareAtlas",
-        "version": "2026.02.08",
-        "hosted": "AKS1",
-        "repo": "https://github.com/UNDP-Data/geo-careatlas",
+        "auth": {
+            "user": user,
+            "email": email,
+            "teams": groups.split(",") if groups else []
+            
+        },
+        "version": "2026.02.09",
+        "env": os.getenv("APP_ENV", "dev"),
         "container_path": os.getcwd()
     }
 
 @app.get("/health")
 async def health():
-    return {"status": "up", "environment": "production"}
+    return {"status": "up"}
